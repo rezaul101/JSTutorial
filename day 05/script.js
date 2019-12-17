ff9e6a24f67efb7f6b5f75db7efc1a0236fa0851
Md.Rezaul Karim <rezaul181211@outlook.com>
2019-12-17 18:02:20 +0600

    13-15

---
+++ day 05/script.js
@@ -0,0 +1,17 @@
+3+4;
+if( 3+4 === 7){
+	//alert("you're smart");
+}
+
+function sayHello(){
+	console.log("Hello");
+}
+//sayHello();
+
+function multiply(a,b){
+	return a*b;
+}
+multiply(5, 10);
+
+
+
