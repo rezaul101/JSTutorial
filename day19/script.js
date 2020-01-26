7a5fa1dcd771d259ece01fd9df180b30a1f591bf
Md.Rezaul Karim <rezaul181211@outlook.com>
2020-01-26 18:41:12 +0600

    Advance Array

---
+++ day19/script.js
@@ -0,0 +1,30 @@
+
+[2] === [2] // false
+{} === {} // false
+
+//#2 what is the value of property a for each object.
+const object1 = { a: 5 }; // 4
+const object2 = object1; // 4
+const object3 = object2; // 4
+const object4 = { a: 5}; // 5
+object1.a = 4;
+
+
+class Animal {
+	constructor(name, type, color) {
+		this.name = name;
+		this.color = color;
+		this.type = type;
+	}
+}
+
+class Mamal extends Animal {
+	constructor(name, type, color) {
+		super(name, type, color)
+	}
+	sound() {
+		console.log(`Moooo I'm ${this.name} and I'm a ${this.color} ${this.type}`);
+	}
+}
+
+const cow = new Mamal('Shelly', 'cow', 'brown');
