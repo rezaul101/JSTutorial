4e67ce796c6a459d4dc0306ee5122645bdf6ae9e
Md.Rezaul Karim <rezaul181211@outlook.com>
2020-01-27 18:29:46 +0600

    ES7

---
+++ day20/script.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const result = fruits.includes("Mango");
+document.getElementById("demo").innerHTML = result;
+
+const Cal = (x) =>x**2;
+const claResult = Cal(5);
+document.getElementById("demo2").innerHTML = claResult;
